5c5d563dc37c913f104df965bf6270798ba4b602
Potoco97 <dnflskfk664@naver.com>
2025-07-04 16:41:47 +0900

    07.04

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Button, Container, Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { addCount, desCount, sortName } from '../store';
+import { addCount, desCount, removeItem, sortName } from '../store';
 
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add" />
 
 const Cart = () => {
 
@@ -19,6 +20,7 @@ const Cart = () => {
                         <th>상품명</th>
                         <th>가격</th>
                         <th>수량</th>
+                        <th>삭제</th>
                     </tr>
                 </thead>
 
@@ -27,13 +29,17 @@ const Cart = () => {
                         cart.map((v, i) =>
                             <tr key={i}>
                                 <td>{v.id}</td>
-                                <td><img src={process.env.PUBLIC_URL+v.img} alt="" /></td>
+                                <td><img src={process.env.PUBLIC_URL + v.img} alt="" /></td>
                                 <td>{v.item}</td>
                                 <td>{v.price.toLocaleString()}원</td>
                                 <td>
-                                    <button onClick={()=>{dispatch(desCount(i));}}>-</button>
+                                    <button onClick={() => { dispatch(desCount(i)); }}>-</button>
                                     <span className='px-2'>{v.amount}</span>
-                                    <button onClick={()=>{dispatch(addCount(i));}}>+</button></td>
+                                    <button onClick={() => { dispatch(addCount(i)); }}>+</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => { dispatch(removeItem(v.id)); }}>x</button>
+                                </td>
                             </tr>
                         )
                     }
@@ -41,7 +47,7 @@ const Cart = () => {
                 </tbody>
 
             </Table>
-            <Button variant="outline-primary"onClick={()=>{
+            <Button variant="outline-primary" onClick={() => {
                 dispatch(sortName())
             }}>이름순정렬</Button>{''}
         </Container>
--- src/store.js
+++ src/store.js
@@ -11,22 +11,33 @@ reducers: {
         },
         desCount(state, action) {
             let idx = action.payload;
-            --state[idx].amount;
-        },
-        removeCount(state, action) {
-            let idx = state.findIndex(data => data.id === action.payload)
-            --state[idx].amount
-            if (state[idx].amount < 1) {
+            if (state[idx].amount < 2) {
                 alert('최소 주문 수량은 1개 입니다.');
                 state[idx].amount = 1;
-            }
+            }else{
+                --state[idx].amount;
+            }            
         },
         sortName(state) {
             state.sort((a, b) => a.item > b.item ? 1 : -1,);
         },
+
         addItem(state, action) {
             state.push(action.payload)
         },
+
+        // addItem(state, action) {
+        //     if (action.payload.type === 'addObj') {
+        //         let idx = state.findIndex(a => a.id === action.payload.id)
+        //         if (idx >= 0) {
+        //             state[idx].amount++;
+        //         } else {
+        //             state.push(action.payload);
+        //         }
+        //     }else{
+        //         return state.push(action.payload)
+        //     }
+        // },
         removeItem(state, action) {
             let idx = state.findIndex(a => a.id === action.payload)
             state.splice(idx, 1)
